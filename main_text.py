bfaa1e2aac07aace18a2edadc37d10dbc3837b30
Boris-Elsys <71979318+Boris-Elsys@users.noreply.github.com>
2021-01-22 13:55:49 +0200

    Add files via upload

---
+++ main_text.py
@@ -0,0 +1,5 @@
+from search_text import *
+
+text = "The dog is a domesticated carnivore of the family Canidae."
+search_sentence = input("Enter somethin to search")
+print("Text is now: {search_and_replace_text()search_sentence.split(' '), text}")
