a92e8f7d84f36c5a1e039f951185ecb710ab7370
Boris-Elsys <71979318+Boris-Elsys@users.noreply.github.com>
2021-03-31 13:40:51 +0300

    Add files via upload

---
+++ company.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+
+class Company:
+    def __init__(self, name):
+        self.name = name
+        self.employees = []
+
+    def print_employees(self):
+        for i in self.employees:
+            print(i.name + ", " + i.email)
+
+    def add_employees(self):
+        self.employees.append(Employee(input("name: "), input("email: ")))
+
+class Employee:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.id = self.gen_id()
+
+    def gen_id(self):
+        id = uuid4()
+        return id
+
+    def print_atributes(self):
+        print("name: " + self.name)
+        print("email: " + self.email)
+        print("employee id: " + str(self.id))
+
+georgi = Employee("e", "y")
+georgi.print_atributes()
+
+company = Company("Krastavi Kiselichki")
+company.add_employees()
+company.print_employees()
